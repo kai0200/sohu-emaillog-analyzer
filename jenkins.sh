0f0d8a4d0defca97088af1fa94cd9fd5fe2b0958
CalerWang <kai0200@gmail.com>
2025-11-25 16:50:27 +0800

    Changes to be committed:     modified:   jenkins.sh     修改 > 为 | tee /tmp/a.log

--- jenkins.sh
+++ jenkins.sh
@@ -2,14 +2,12 @@ email_address=$1
 email_other=$2
 email_date=$3
 
-cd /opt/sohu-emaillog-analyzer-test
-#cd /opt/sohu-emaillog-analyzer/
+#cd /opt/sohu-emaillog-analyzer-test
+cd /opt/sohu-emaillog-analyzer/
 
-echo $email_address
-echo $email_other
-echo $email_date
+echo "sender:$email_address rcptto:$email_other  Date:$email_date "
 
-./remote_mail_grep.sh  $email_address $email_date > /tmp/a.log
+./remote_mail_grep.sh  $email_address $email_date | tee /tmp/a.log
 
 #grep $email_other /tmp/b.log > /tmp/c.log
 
