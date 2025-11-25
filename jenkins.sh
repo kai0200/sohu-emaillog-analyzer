c3ed4c48f90c15422e5dc1e03645e52c348b6ad1
CalerWang <kai0200@gmail.com>
2025-11-25 16:16:06 +0800

    Changes to be committed:     modified:   jenkins.sh  修改适合测试使用

--- jenkins.sh
+++ jenkins.sh
@@ -2,15 +2,15 @@ email_address=$1
 email_other=$2
 email_date=$3
 
-cd /opt/sohu-emaillog-analyzer/
+cd /opt/sohu-emaillog-analyzer-test
+#cd /opt/sohu-emaillog-analyzer/
 
-echo $email_address  
+echo $email_address
 echo $email_other
 echo $email_date
 
-./remote_mail_grep.sh  $email_address $email_date > /tmp/b.log
+./remote_mail_grep.sh  $email_address $email_date > /tmp/a.log
 
-grep $email_other /tmp/b.log > /tmp/c.log
+#grep $email_other /tmp/b.log > /tmp/c.log
 
-
-/usr/bin/python3 analyze_logs.py -l /tmp/c.log  -s $email_address -r $email_other
+/usr/bin/python3 analyze_logs.py -l /tmp/a.log  -s $email_address -r $email_other
